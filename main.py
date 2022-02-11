1de4954ab73791abd452dcdb4c9f452420d91171
lampar96 <javier_canada_96@hotmail.com>
2022-02-11 11:10:08 +0100

    Añadidas funciones

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import pandas as pd
 from openpyxl import Workbook, load_workbook
 
-df = pd.read_excel('cmdb_ci_db_instance.xlsx', engine='openpyxl')
+
+def calc_percentage(new_cell, old_cell):
+    percentage = ((new_cell - old_cell)/old_cell)*100
+
+def create_percentage_table():
+    pass
+
+
+df = pd.read_excel('cmdb_ci_db_instance.xlsx')
 num_filas = df.shape[0]
+num_columnas = df.shape[1]
 
 wb = load_workbook('Libro1.xlsx')
 ws = wb.active
-ws['B3'] = 0
+ws['B3'] = num_filas
 wb.save('Libro2.xlsx')
+df = pd.read_excel('Libro2.xlsx')
+num_filas_res = df.shape[0]
+num_columnas_res = df.shape[1]
+#for i in range(num_filas_res):
 
+print("Excel de entrada:", num_filas, num_columnas)
+print("Excel de salida:", num_filas_res,num_columnas_res)
 
-print(df.shape[0])
